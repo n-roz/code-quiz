bcabb9e76b35e8cfea690ef3694e8c4f0e33e0a0
Mona Lisa <naomiroznik@gmail.com>
2022-08-21 12:44:36 -0400

    Adding js function to displayHighScores when called

--- assets/script.js
+++ assets/script.js
@@ -261,3 +261,36 @@ var containerQuestionEl = document.getElementById("question-container");
             
         }
     }  
+
+        // display high score screen from link or when intials are entered
+        var displayHighScores = function() {
+
+            containerHighScoresEl.classList.remove("hide");
+            containerHighScoresEl.classList.add("show");
+            gameover = "true"
+    
+            if (containerEndEl.className = "show") {
+                containerEndEl.classList.remove("show");
+                containerEndEl.classList.add("hide");
+                }
+            if (containerStartEl.className = "show") {
+                containerStartEl.classList.remove("show");
+                containerStartEl.classList.add("hide");
+                }
+                
+            if (containerQuestionEl.className = "show") {
+                containerQuestionEl.classList.remove("show");
+                containerQuestionEl.classList.add("hide");
+                }
+    
+            if (correctEl.className = "show") {
+                correctEl.classList.remove("show");
+                correctEl.classList.add("hide");
+            }
+    
+            if (wrongEl.className = "show") {
+                wrongEl.classList.remove("show");
+                wrongEl.classList.add("hide");
+                }
+            
+        }
