af877ae78aece6bf4c7d65b581e492c01d9b6279
Mona Lisa <naomiroznik@gmail.com>
2022-08-21 12:23:23 -0400

    Adding questions for the quiz

--- assets/script.js
+++ assets/script.js
@@ -22,9 +22,41 @@ var containerQuestionEl = document.getElementById("question-container");
       var gameover
       timerEl.innerText = 0;
 
-      //High Score Array
+      // high Score Array
       var HighScores = [];
 
-       //assign array details for questions 
+       // assign array details for questions 
       var arrayShuffledQuestions
       var QuestionIndex = 0
+
+    // questions for the quiz
+    var questions = [
+        { q: 'Arrays in Javascript can be used to store __________.', 
+          a: '4. all of the above', 
+          choices: [{choice: '1. numbers'}, {choice: '2. booleans'}, {choice: '3. strings'}, {choice: '4. all of the above'}]
+        },
+        { q: 'Inside which HTML element do we put the javascript?', 
+          a: '3. <script>', 
+          choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
+        },
+        { q: 'In the code -- setinterval(time(),1000) -- what is time()?', 
+          a: '1. callback function', 
+          choices: [{choice: '1. callback function'}, {choice: '2. undefined'}, {choice: '3. variable'}, {choice: '4. all of the above'}]
+        },
+        { q: 'What syntax would call a function?', 
+          a: '4. function()', 
+          choices: [{choice: '1. var function'}, {choice: '2. function'}, {choice: '3. call function'}, {choice: '4. function()'}]
+        },
+        { q: 'When did javascript first appear?', 
+          a: '1. 1995', 
+          choices: [{choice: '1. 1995'}, {choice: '2. 2005'}, {choice: '3. 2015'}, {choice: '4. 2025'}]
+        },
+        { q: 'What does DOM stand for?', 
+          a: '2. Document Object Model', 
+          choices: [{choice: '1. Document Object Modification'}, {choice: '2. Document Object Model'}, {choice: '3. Donuts Only, Mom'}, {choice: '4. Desktop On Module'}]
+        },
+        { q: 'What is getItem commonly used for?', 
+          a: '2. local storage', 
+          choices: [{choice: '1. creating a function'}, {choice: '2. local storage'}, {choice: '3. online shopping'}, {choice: '4. naming a variable'}]
+        },
+      ];
